0b9b16d1e0ea323994ebe675134b540bc4255311
SamaneSharifiMonfared <samane.sharify@gmail.com>
2023-01-12 23:52:29 +0000

    delete pokemon

--- app/Controllers/Http/PokemonController.ts
+++ app/Controllers/Http/PokemonController.ts
@@ -4,7 +4,8 @@ import Pokemons from 'App/Models/Pokemons'
 
 export default class PokemonController {
   public async index() {
-    return Pokemons.all() //returining all of pokemons from DB
+    const pokemons = await Pokemons.all()
+    return pokemons //returining all of pokemons from DB
   }
 
   public async store({ request, response }: HttpContextContract) {
@@ -25,7 +26,22 @@ export default class PokemonController {
 
   public async update({ request, response, params}: HttpContextContract){
     //update a pokemon by id
-    const pokemon = Pokemons.findOrFail(params.id)
-    return pokemon
+    const pokemon = await Pokemons.findOrFail(params.id)
+    pokemon.name = request.input('name')
+    pokemon.color = request.input('color')
+    pokemon.description = request.input('description')
+    pokemon.save()
+    return response.status(202).send(pokemon)
+
+    //response status codes 200 : default . successful
+    //response status codes 204 : no content
+    //response status codes 202 : updated
+
+  }
+
+  public async delete({request, response, params}: HttpContextContract){
+
+    const pokemon = await Pokemons.findOrFail(params.id)
+    await pokemon.delete()
   }
 }
